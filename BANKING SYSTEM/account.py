4a0f8789ad02392eb9ca48bec459f7cd919c3828
techmadesh007 <techmadesh0@gmail.com>
2026-08-09 11:15:53 +0530

    Update account.py

--- BANKING SYSTEM/account.py
+++ BANKING SYSTEM/account.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class Account(ABC):
 
     def __init__(self, account_no, name, pin, balance):
@@ -7,6 +8,7 @@ class Account(ABC):
         self.__name = name
         self.__pin = pin
         self.__balance = balance
+        self.transactions = []
 
     def get_account_no(self):
         return self.__account_no
@@ -23,14 +25,22 @@ class Account(ABC):
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
+            self.transactions.append("Deposited: " + str(amount))
             return True
         return False
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self.__balance:
             self.__balance -= amount
+            self.transactions.append("Withdrawn: " + str(amount))
             return True
         return False
+
+    def add_transaction(self, message):
+        self.transactions.append(message)
+
+    def get_transactions(self):
+        return self.transactions
 
     @abstractmethod
     def account_type(self):
